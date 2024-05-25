dff2bce5cca2a878d1c54683f55a86d613613dfe
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-05-25 10:49:25 +0800

    Update 03_Clustering_Results.py

--- streamlit_demo/pages/03_Clustering_Results.py
+++ streamlit_demo/pages/03_Clustering_Results.py
@@ -4,8 +4,8 @@ st.title("CUSTOMER SEGMENTATION")
 st.write("Based on AAC Credit Card Transactions as of December 7, 2021")
 
 
-st.header("Elbow Method and Silhouette Score forOptimal Number of Cluster")
-st.image("streamlit_demo/images/Cluster.png")
+st.header("Elbow Method and Silhouette Score for Optimal Number of Cluster")
+st.image("streamlit_demo/images/cluster.png")
 st.text("1 | Based on our clustering analysis , the elbow method suggests 3 optimal")
 st.text("    clusters while the silhouette score indicates 5.") 
 st.text("2 | The 3-cluster model offers a simpler segmentation of high, medium, and")
@@ -13,3 +13,6 @@ st.text("    low-value customers. While the 5-cluster model provides more detail
 st.text("    insights, identifying 5 distinct subgroups")
 
 
+st.title("Meet the Clusters")
+st.text("1 | Based on our clustering analysis , the elbow method suggests 3 optimal")
+st.image("streamlit_demo/images/Top.png")
