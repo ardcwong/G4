9e543029e14c9e173c4f566fb4566eee847a44aa
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-05-25 10:33:57 +0800

    Create 03_Clustering_Results.py

---
+++ streamlit_demo/pages/03_Clustering_Results.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Customer Demographics")
+st.write("Based on AAC Credit Card Transactions as of December 7, 2021")
+
+
+st.header("Gender")
+st.text("AAC Credit Card Holders demographic shows a strong gender disparity with males")
+st.text("comprising 94% and females just 6% of the cardholders.")
+st.image("streamlit_demo/images/Gender.png")
