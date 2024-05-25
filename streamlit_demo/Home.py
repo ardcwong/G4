a74303a29140b6c064b697e571ca978b3f507f54
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-05-25 11:56:49 +0800

    Update Home.py

--- streamlit_demo/Home.py
+++ streamlit_demo/Home.py
@@ -5,15 +5,15 @@ st.title("DECODE - ENCODE : Driving business growth through Clustering and Linea
 
 st.header("About The Challenge")
 st.image("streamlit_demo/images/AAC.png")
-st.write("Adobo Advantage Cards (AAC), a credit card company, collected comprehensive dataset of credit card transactions which includes: 1) Transaction Details, 2) Customer Demographics, and 3) Amount spent in various categories")
+st.write("Adobo Advantage Cards (AAC), a credit card company, collected comprehensive dataset of credit card transactions which includes: 1) Transaction Details, 2) Customer Demographics, and 3) Amount spent in various categories.")
 
 st.header("DATASQU4D")
 st.image("streamlit_demo/images/DS.png")
 st.write("DATASQU4D, one of the data science consultancy teams, was challenged to turn this data into actionable strategies that shall enhance customer satisfaction and drive business growth.")
 
 st.text("")
-st.text("____________________________________________________________________________________")
-st.write("Arden Sarmiento, Austine Wong, Bea Reyes, Evian Aguilar, Japhet Pamonag")
+st.text("")
+st.write("Arden Sarmiento | Austine Wong | Bea Reyes | Evian Aguilar | Japhet Pamonag")
 
 
 
