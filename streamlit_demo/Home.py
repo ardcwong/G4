1b792cccf285d73892b11c959aa4d0f5aa4552a1
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-05-25 11:50:13 +0800

    Update Home.py

--- streamlit_demo/Home.py
+++ streamlit_demo/Home.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
-st.title("Home")
-st.write("Provide a short description of the problem and your objective.")
-st.write("Provide a list of your names and github handles")
+st.title("DECODE - ENCODE : Driving business growth through Clustering and Linear Regression Modelling")
+
+st.title("About the Challenge")
+st.header("Adobo Advantage Cards (AAC)")
+st.image("streamlit_demo/images/AAC.png")
+st.write("Adobo Advantage Cards (AAC), a credit card company, collected comprehensive dataset of credit card transactions which includes:")
+st.text("Transaction Details")
+st.text("Customer Demographics")
+st.text("Amount spent in various categories")
+
+st.header("DATASQU4D")
+st.image("streamlit_demo/images/DS.png")
+st.write("DATASQU4D, one of the data science consultancy teams, was challenged to turn this data into actionable strategies that shall enhance customer satisfaction and drive business growth.")
+
+
+
+
+st.write("Arden Sarmiento")
+st.write("Austine Wong")
+st.write("Bea Reyes")
+st.write("Evian Aguilar")
+st.write("Japhet Pamonag")
+
+
 
