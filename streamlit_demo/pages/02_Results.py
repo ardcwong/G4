9f0abcd3002d697d0003a2388eb20a86d1e35c8c
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-05-25 10:03:18 +0800

    Update 02_Results.py

--- streamlit_demo/pages/02_Results.py
+++ streamlit_demo/pages/02_Results.py
@@ -6,20 +6,20 @@ st.write("Here you can put each of your key results.")
 
 st.header("Gender")
 st.text("AAC Credit Card Holders demographic shows a strong gender disparity with males comprising 94% and females just 6% of the cardholders.")
-#st.image("streamlit_demo/images/Gender.png")
+st.image("streamlit_demo/images/Gender.png")
 
 st.header("Active Customers per year")
-#st.image("streamlit_demo/images/Active.png")
+st.image("streamlit_demo/images/Active.png")
 st.text("Compared to 78 users in 2020, there was an increase in active customers in 2021, with 88 active users.")
 
 
 st.header("Age")
-#st.image("streamlit_demo/images/total_spending.png")
+st.image("streamlit_demo/images/Age.png")
 st.text("The age range of cardholders is between 51 and 90, with the mean age being 67.
 ")
 
 st.header("Generation Distribution")
-#st.image("streamlit_demo/images/Generation.png")
+st.image("streamlit_demo/images/Generation.png")
 st.text("The majority of these customers are from the Baby Boomer generation.")
 
 
