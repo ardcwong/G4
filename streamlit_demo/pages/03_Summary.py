28a062ce34766ca043c2e26e55fe0607338c8ca0
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-05-25 11:18:23 +0800

    Update 03_Summary.py

--- streamlit_demo/pages/03_Summary.py
+++ streamlit_demo/pages/03_Summary.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-st.title("Summary")
+st.title("Clustering Summary")
+st.write("Here you can provide a short summary and recommendations")
+
+
+st.header("Top Customers")
+st.image("streamlit_demo/images/top.png")
+st.text("Top and Engaged customers spend the most in physical grocery stores (grocery_pos).")
+
+st.header("Engaged Customers")
+st.image("streamlit_demo/images/engaged.png")
+st.text("Top and Engaged customers spend the most in physical grocery stores (grocery_pos).")
+st.header("At-risk Customers")
+st.image("streamlit_demo/images/risk.png")
+st.text("Top and Engaged customers spend the most in physical grocery stores (grocery_pos).")
+
+
+st.title("Supervised Learning: Linear Regression")
 st.write("Here you can provide a short summary and recommendations")
